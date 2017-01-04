3edb6c7961901fd41c1ef1d6a82725b18e23f5d5
tzmanics <tzmanics@gmail.com>
2017-01-03 23:58:50 -0500

    add the toshi editor edits

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,28 @@
 import { Component } from '@angular/core';
 
+export class Toshi {
+  rating: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template:`
+    <h1>{{title}}</h1>
+    <h2>How about {{toshi.name}}</h2>
+    <div><label>rating: </label>{{toshi.rating}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="toshi.name" placeholder="name">
+    </div>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent {
+  title = 'All the Toshis!';
+
+  toshi: Toshi = {
+    rating: 95,
+    name: 'Toshi in Pajamas'
+  };
+}
